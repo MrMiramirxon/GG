d5c55bede71deae757c7261e25e3fc893c9ca149
MrMiramirxon <mirabzalova62@gmail.com>
2024-09-12 18:52:56 +0500

    vorisbekkkkk

---
+++ functions/server.mjs
@@ -0,0 +1,34 @@
+import express from 'express';
+import { faker } from '@faker-js/faker';
+
+const app = express();
+const port = 8888;
+
+
+function createRandomUser() {
+  return {
+    userId: faker.string.uuid(),
+    username: faker.internet.userName(),
+    email: faker.internet.email(),
+    avatar: faker.image.avatar(),
+    password: faker.internet.password(),
+    birthdate: faker.date.birthdate(),
+    registeredAt: faker.date.past(),
+  };
+}
+
+
+app.get('/api/users', (req, res) => {
+  const numberOfUsers = parseInt(req.query.count) || 1000; 
+  const users = Array.from({ length: numberOfUsers }, createRandomUser);
+  res.json(users);
+});
+app.get('/api/yyy', (req, res) => {
+  const numberOfUsers = parseInt(req.query.count) || 1000; 
+  const users = Array.from({ length: numberOfUsers }, createRandomUser);
+  res.json(users);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
---
+++ routes/cars.mjs
@@ -0,0 +1,15 @@
+import express from 'express'
+
+const router = express.Router()
+
+router.get("/all",(req,res) => {
+    res.send("brentford")
+})
+router.get("/all",(req,res) => {
+    res.send("dsfsdf")
+})
+router.get("/all",(req,res) => {
+    res.send("dsfsdf")
+})
+
+export default router
